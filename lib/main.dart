31f005be01c66600a727f537561b7f121b78de37
jarb16633 <69179973+jarb16633@users.noreply.github.com>
2022-03-06 15:45:22 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:kkuvrun_mobileapp/screens/activity.dart';
+import 'package:kkuvrun_mobileapp/screens/community.dart';
+import 'package:kkuvrun_mobileapp/screens/home.dart';
+import 'package:kkuvrun_mobileapp/screens/notification.dart';
+import 'package:kkuvrun_mobileapp/screens/run.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 0;
+  final screens = [
+    const HomePage(),
+    const ActivityPage(),
+    const RunPage(),
+    const CommunityPage(),
+    const NotificationPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.black38,
+        iconSize: 30,
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() => currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.leaderboard_outlined),
+            label: 'Feed',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_run_outlined),
+            label: 'Run',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_outlined),
+            label: 'Club',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail_outlined),
+            label: 'Notification',
+          ),
+        ],
+      ),
+    );
+  }
+}
